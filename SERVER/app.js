9f5d580d11bb8e9ac191b2d65dc07fac8608005a
srinidhisg88 <srinidhisg1@gmail.com>
2024-07-12 10:31:01 +0530

    .2

--- SERVER/app.js
+++ SERVER/app.js
@@ -1,6 +1,8 @@
 const express=require("express")
 const app=express()
 const cors=require("cors")
+app.use(express.urlencoded())
+ app.use(cors())
 app.use(express.json())
 app.listen(8080,()=>{
     console.log('listening to port 8080')
